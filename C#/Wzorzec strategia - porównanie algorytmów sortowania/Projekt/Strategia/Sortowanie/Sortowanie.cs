ebbac03cf1d4021e2b09e7ea0d7c4b716aa10a2a
KamilKas1999 <kamilkasprzak1999@gmail.com>
2020-05-28 15:08:32 +0200

    28.05.2020

---
+++ C#/Wzorzec strategia - porównanie algorytmów sortowania/Projekt/Strategia/Sortowanie/Sortowanie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategia
+{
+    class Sortowanie
+    {
+        private StrategiaSortowania strategia;
+       
+            public void sortuj(int[] tablica)
+            {
+                strategia.sortuj(tablica);
+            }
+            public void ustawSortowanie(StrategiaSortowania s)
+            {
+                strategia = s;
+            }
+           
+        }
+    
+}
+
