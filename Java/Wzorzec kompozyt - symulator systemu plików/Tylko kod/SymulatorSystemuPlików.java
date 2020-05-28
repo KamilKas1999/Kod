0ae8ddd931f212c784ab415260ecdb1c7edc948b
Kamil <56410411+KamilKas1999@users.noreply.github.com>
2020-05-28 16:05:56 +0200

    Update SymulatorSystemuPlików.java

--- Java/Wzorzec kompozyt - symulator systemu plików/Tylko kod/SymulatorSystemuPlików.java
+++ Java/Wzorzec kompozyt - symulator systemu plików/Tylko kod/SymulatorSystemuPlików.java
@@ -5,7 +5,7 @@ public class SymulatorSystemuPlików {
     public static void main(String[] args) {
 
         System.out.println("Witaj w prostym symulatorze cmd!");
-        System.out.println("Możesz tu używać takich komedna jak:");
+        System.out.println("Możesz tu używać takich komend jak:");
         System.out.println("dir");
         System.out.println("cd ..");
         System.out.println("cd <nazwa katalogu>");
